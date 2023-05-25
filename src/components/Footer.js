cd30485f2fa05ca2a2de825dea7e81da14d2cc53
Drikadev <126597972+drika-dev@users.noreply.github.com>
2023-05-25 13:05:07 -0300

    Ajeitando as cores e imagem

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from './Footer.module.css';
 const Footer = () => {
-    return <footer className={styles.footer}> Escrever alguma coisa
+    return <footer className={styles.footer}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 <p>Drika-dev&copy; 2023</p>
             </footer>;
 
--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -1,6 +1,7 @@
 import styles from "./Cadastro.module.css"
 import { useState } from "react";
-import { useInsertUser } from "../../hooks/userRegister"
+import { useInsertUser } from "../../hooks/userRegister";
+import { useNavigate } from "react-router-dom";
 
 const Cadastro = () => {
     const [displayName, setDisplayName] = useState("");
@@ -8,42 +9,31 @@ const Cadastro = () => {
     const [telefone, setTelefone] = useState("");
     const [formError, setFormError] = useState("");
 
-    /*const navigate = useNavigate();
+    const navigate = useNavigate();
 
-    */const { insertUser, response } = useInsertUser("registercontacts");
+    const { insertUser, response } = useInsertUser("registercontacts");
     const handleSubmit = (e) => {
         e.preventDefault();
         setFormError("");
-        console.log({
+        insertUser({
             displayName,
             email,
             telefone
         });
-        /*
-            if(formError) return
-        
-            insertDocument({
-              title,
-              image,
-              body,
-              tags: tagsArray,
-              uid: user.uid,
-              createdBy: user.displayName,
-            });
-        
-            // redirect to home page
-            navigate("/");*/
+
+        // redirect to home page
+        navigate("/");
     };
 
     return (
-        <div className={styles.contactus}>
-            <h2> Cadastre seu contato para receber
-                <p></p>nossas notícias e atualizações</h2>
-            <br />
-            <br />
-            <br />
-            <br />
-            <form onSubmit={handleSubmit}>
+        <div>
+            <form onSubmit={handleSubmit} class="contact-us">
+                <h2> Cadastre seu contato para receber
+                    <p></p>nossas notícias e atualizações</h2>
+                <br />
+                <br />
+                <br />
+                <br />
                 <input placeholder="Nome" required="" type="text" name="displayName" onChange={(e) => setDisplayName(e.target.value)} value={displayName} />
                 <input type="email" name="email" required placeholder="E-mail do usuário" onChange={(e) => setEmail(e.target.value)} value={email} />
                 <input type="tel" name="telefone" required placeholder="Telefone" onChange={(e) => setTelefone(e.target.value)} value={telefone} />
